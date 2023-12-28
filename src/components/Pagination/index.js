f644b24f165644e0df2138ab58d9c8d53a7ca25e
ardamuh <arda.muhamdn@gmail.com>
2023-12-28 18:21:32 +0700

    first commit

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+
+const Pagination = (props) => {
+  const {
+    currentDataLength,
+    handlePageClick,
+    pages,
+    page = 1,
+    limit = 0,
+    total,
+  } = props;
+  const startDataPage = limit * (page - 1) + 1;
+  const endDataPage =
+    pages === page && currentDataLength < limit ? total : limit * page;
+
+  return (
+    <>
+      <p className="float-start">{`Showing ${startDataPage} to ${endDataPage} of ${total} entries`}</p>
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={
+          <span role="button" className="page-link">
+            ...
+          </span>
+        }
+        breakClassName={"page-item"}
+        pageCount={pages}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination float-end"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        activeClassName={"active"}
+        forcePage={page - 1}
+      />
+    </>
+  );
+};
+
+export default Pagination;
---
+++ src/pages/events/form.js
@@ -0,0 +1,270 @@
+import React from "react";
+import {
+  CloseButton,
+  Col,
+  Figure,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import Button from "../../components/Button";
+import TextInputWithLabel from "../../components/TextInputWithLabel";
+import SelectBox from "../../components/SelectBox";
+import { config } from "../../configs";
+
+export default function EventsForm({
+  handleSubmit,
+  form,
+  handleChange,
+  isLoading,
+  edit,
+  lists,
+  handlePlusKeyPoint,
+  handleChangeKeyPoint,
+  handleMinusKeyPoint,
+  handlePlusTicket,
+  handleMinusTicket,
+  handleChangeTicket,
+}) {
+  const statusEvent = [
+    {
+      value: "Published",
+      label: "Published",
+      name: "statusEvent",
+      target: { value: "Published", name: "statusEvent" },
+    },
+    {
+      value: "Draft",
+      label: "Draft",
+      name: "statusEvent",
+      target: { value: "Draft", name: "statusEvent" },
+    },
+  ];
+  return (
+    <Form className="mb-2">
+      <Row>
+        <Col>
+          <TextInputWithLabel
+            placeholder={"Masukan judul"}
+            label={"Judul"}
+            name="title"
+            value={form.title}
+            type="text"
+            onChange={handleChange}
+          />
+        </Col>
+        <Col>
+          <TextInputWithLabel
+            placeholder={"Masukan tagline"}
+            label={"Tagline"}
+            name="tagline"
+            value={form.tagline}
+            type="text"
+            onChange={handleChange}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <TextInputWithLabel
+            placeholder={"Masukan tanggal acara"}
+            label={"Tanggal"}
+            name="date"
+            value={form.date}
+            type="datetime-local"
+            onChange={handleChange}
+          />
+        </Col>
+        <Col>
+          <SelectBox
+            label={"Category"}
+            placeholder={"Masukan kategori"}
+            name="category"
+            value={form.category}
+            options={lists.categories}
+            isClearable={true}
+            handleChange={(e) => handleChange(e)}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <TextInputWithLabel
+            placeholder={"Masukan about"}
+            label={"About"}
+            name="about"
+            value={form.about}
+            type="text"
+            onChange={handleChange}
+          />
+        </Col>
+        <Col>
+          <TextInputWithLabel
+            placeholder={"Masukan tempat acara"}
+            label={"Tempat acara"}
+            name="venueName"
+            value={form.venueName}
+            type="text"
+            onChange={handleChange}
+          />
+        </Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Form.Group as={Row} controlId="formMapEmbedCode">
+          <Form.Label column sm={2}>
+            Google Maps Embed Code
+          </Form.Label>
+          <Row>
+            <Col sm={60}>
+              <FormControl
+                as="textarea"
+                rows={3}
+                name="mapEmbedCode"
+                value={form.mapEmbedCode || ""} // ensure the value is controlled
+                onChange={handleChange}
+                placeholder="Paste your Google Maps embed code here"
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+      </Row>
+
+      <Form.Label>Key Point</Form.Label>
+      <Row>
+        {form.keyPoint.map((key, index) => (
+          <Col sm={6}>
+            <InputGroup className="mb-3" key={index}>
+              <FormControl
+                placeholder="Masukan keypoint"
+                value={key}
+                type="text"
+                name="key"
+                onChange={(e) => {
+                  handleChangeKeyPoint(e, index);
+                }}
+              />
+              {index !== 0 && (
+                <InputGroup.Text id="basic-addon2">
+                  <CloseButton onClick={() => handleMinusKeyPoint(index)} />
+                </InputGroup.Text>
+              )}
+            </InputGroup>
+          </Col>
+        ))}
+      </Row>
+
+      <Button
+        className="mb-3"
+        variant="success"
+        action={handlePlusKeyPoint}
+        size="sm"
+      >
+        Tambah keypoint
+      </Button>
+
+      <Row>
+        <Col>
+          <SelectBox
+            label={"Talent"}
+            placeholder={"Masukan talent"}
+            name="talent"
+            value={form.talent}
+            options={lists.talents}
+            isClearable={true}
+            handleChange={(e) => handleChange(e)}
+          />
+        </Col>
+        <Col>
+          <TextInputWithLabel
+            placeholder={"Masukan Avatar"}
+            label={"Cover"}
+            name="avatar"
+            // value={form.avatar}
+            type="file"
+            onChange={handleChange}
+          />
+          {form.avatar !== "" && (
+            <div>
+              <Figure>
+                <Figure.Image
+                  width={171}
+                  height={180}
+                  alt="171x180"
+                  src={`${config.api_image}/${form.avatar}`}
+                />
+
+                <Figure.Caption>Perview image cover</Figure.Caption>
+              </Figure>
+            </div>
+          )}
+        </Col>
+      </Row>
+
+      <Form.Label>Tiket</Form.Label>
+
+      {form.tickets.map((tic, index) => (
+        <Row>
+          <Col sm={6}>
+            <TextInputWithLabel
+              placeholder={"Masukan tipe tiket"}
+              label={"Tipe"}
+              name="type"
+              value={tic.type}
+              type="text"
+              onChange={(e) => handleChangeTicket(e, index)}
+            />
+          </Col>
+          <Col sm={6}>
+            <TextInputWithLabel
+              placeholder={"Masukan Harga"}
+              label={"Harga"}
+              name="price"
+              value={tic.price}
+              type="number"
+              onChange={(e) => handleChangeTicket(e, index)}
+            />
+          </Col>
+          <Col sm={6}>
+            <TextInputWithLabel
+              placeholder={"Masukan jumlah tiket"}
+              label={"Stock"}
+              name="stock"
+              value={tic.stock}
+              type="number"
+              onChange={(e) => handleChangeTicket(e, index)}
+            />
+          </Col>
+          <Col sm={index !== 0 ? 5 : 6}>
+            <SelectBox
+              label={"Status Ticket"}
+              placeholder={"Masukan Status Ticket"}
+              value={lists.statusEvent}
+              options={statusEvent}
+              isClearable={true}
+              handleChange={(e) => handleChange(e)}
+            />
+          </Col>
+          {index !== 0 && (
+            <Col
+              sm={1}
+              className="d-flex justify-content-end align-items-center"
+            >
+              <CloseButton onClick={() => handleMinusTicket(index)} />
+            </Col>
+          )}
+        </Row>
+      ))}
+      <div className="mb-3">
+        <Button variant="success" action={handlePlusTicket} size="sm">
+          Tambah Ticket
+        </Button>
+      </div>
+
+      <Button variant="primary" action={handleSubmit} loading={isLoading}>
+        {edit ? "Ubah" : "Simpan"}
+      </Button>
+    </Form>
+  );
+}
